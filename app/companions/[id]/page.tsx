78e16f8e19242e6306ccb70f8abafcb61ef9bf44
rabbisiddique <rabbysanny64@gmail.com>
2025-09-02 15:55:20 +0600

    first commit

---
+++ app/companions/[id]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CompanionsSession = () => {
+  return <div>CompanionsSession</div>;
+};
+
+export default CompanionsSession;
---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const Page = () => {
+  return (
+    <>
+      <p className="text-2xl text-green-400 underline">
+        Welcome to the Sass application
+      </p>
+      <Button>Let`s get started</Button>
+    </>
+  );
+};
+
+export default Page;
