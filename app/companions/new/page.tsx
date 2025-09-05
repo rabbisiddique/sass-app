038f211d8f11eeeaba3468ca85c6d36e3e380273
rabbisiddique <rabbysanny64@gmail.com>
2025-09-05 15:23:59 +0600

    implement subscritions

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -9,33 +9,36 @@ import React from "react";
 const NewCompanion = async () => {
   const { userId } = await auth();
   if (!userId) redirect("/sign-in");
+
   const canCreateCompanion = await newCompanionPermissions();
+
   return (
     <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
       {canCreateCompanion ? (
         <article className="w-full gap-4 flex flex-col">
           <h1>Companion Builder</h1>
+
           <CompanionForm />
         </article>
       ) : (
         <article className="companion-limit">
           <Image
             src="/images/limit.svg"
-            alt="companion limit reached"
+            alt="Companion limit reached"
             width={360}
             height={230}
           />
           <div className="cta-badge">Upgrade your plan</div>
           <h1>You`ve Reached Your Limit</h1>
           <p>
-            You`ve Reached Your companion limit. Upgrade to create more
+            You`ve reached your companion limit. Upgrade to create more
             companions and premium features.
           </p>
           <Link
-            href={"/subscriptions"}
+            href="/subscriptions"
             className="btn-primary w-full justify-center"
           >
-            Upgrade my plan
+            Upgrade My Plan
           </Link>
         </article>
       )}
--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -96,23 +96,31 @@ export const getUserCompanion = async (userId: string) => {
 export const newCompanionPermissions = async () => {
   const { userId, has } = await auth();
   const supabase = createSupaBaseClient();
+
   let limit = 0;
+
   if (has({ plan: "pro" })) {
     return true;
-  } else if (has({ feature: "3_companions_limit" })) {
+  }
+
+  if (has({ feature: "3_companion_limit" })) {
     limit = 3;
-  } else if (has({ feature: "10_companions_limit" })) {
+  } else if (has({ feature: "10_companion_limit" })) {
     limit = 10;
   }
+
   const { data, error } = await supabase
     .from("companions")
     .select("id", { count: "exact" })
     .eq("author", userId);
+
   if (error) throw new Error(error.message);
+
   const companionCount = data?.length;
+
   if (companionCount >= limit) {
-    return true;
-  } else {
     return false;
+  } else {
+    return true;
   }
 };
